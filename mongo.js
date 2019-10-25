b21dad4d7a3606f5475e6f3b56790fccf74b3992
Jan Nyman <jan.nyman@comiq.fi>
2019-10-25 21:05:17 +0300

    Exercise 3.22

--- mongo.js
+++ mongo.js
@@ -1,43 +1,43 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://fullstack:${password}@cluster0-vxgg4.mongodb.net/person-app?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0-vxgg4.mongodb.net/person-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length > 3) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-    person.save().then(response => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`);
-        mongoose.connection.close();
-    })
+  person.save().then(() => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 
 }
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 }
